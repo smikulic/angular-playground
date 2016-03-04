4e597ff4ee80e63111939a148ba374d91dc78abf
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2016-03-04 16:02:28 +0100

    Add navigation and steps

---
+++ app/js/controllers.js
@@ -0,0 +1,20 @@
+var bookATigerApp = angular.module('bookATigerApp', []);
+
+bookATigerApp.controller('BookingStepsCtrl', function ($scope) {
+  $scope.steps = [
+    {'name': 'Date and zip code',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Clening details',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'Address',
+     'snippet': 'The Next, Next Generation tablet.'},
+     {'name': 'Payment',
+      'snippet': 'The Next, Next Generation tablet.'}
+  ];
+
+  $scope.selected = 0;
+
+  $scope.setActive = function(index) {
+    $scope.selected = index;
+  };
+});
