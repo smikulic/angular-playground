24ab4b27637ece14efd11d00653be2ef9f343ba8
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2016-03-06 13:16:22 +0100

    Add pricing details

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,21 +13,41 @@ bookATigerApp.controller('BookingStepsCtrl', function ($scope) {
     {'name': 'every 2 weeks','price': '13,90', 'bestseller': false},
     {'name': 'every 4 weeks','price': '13,90', 'bestseller': false}
   ];
+  $scope.offers = [
+    {'name': 'Economy', 'price': '13,90',
+    'description':'Economy Offer: Your home will be cleaned!'},
+    {'name': 'Economy Plus','price': '14,90',
+    'description':'Economy Plus Offer: Your home will be cleaned by an experienced Tiger!'},
+    {'name': 'Premium','price': '15,90',
+    'description':'Premium Offer: Your home will be cleaned by a Premium Tiger!'},
+    {'name': 'Premium Plus','price': '18,90',
+    'description':'Premium Plus Offer: Your home will be cleaned by a Premium Plus Tiger!'}
+  ];
   $scope.selected = 0;
   $scope.openFrequencySelector = false;
+  $scope.offerDetails = false;
+  $scope.pricingSelected = 1;
 
   $scope.updateBooking = function(booking, nextStep) {
     $scope.selected = nextStep;
-    console.log(booking)
   };
 
   $scope.toggleFrequencySelect = function() {
     $scope.openFrequencySelector = !$scope.openFrequencySelector;
+    $scope.offerDetails = false;
   }
 
   $scope.selectFrequency = function(booking, frequency) {
     $scope.booking.frequency = frequency;
     $scope.toggleFrequencySelect();
-    console.log(booking)
+    $scope.offerDetails = true;
+  }
+
+  $scope.changePricing = function(pricingOption) {
+    $scope.pricingSelected = pricingOption;
+  }
+
+  $scope.done = function(booking) {
+    alert("Congratulations! You made a reservation for " + booking.time + ", price: " + $scope.offers[$scope.pricingSelected].price)
   }
 });
